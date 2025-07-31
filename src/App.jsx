686435e915648c919e0ec360ee2489e1933730f2
USER <rhs9827@naver.com>
2025-07-31 10:54:21 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Counter from "./components/Counter";
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
-      {Counter()}
+      <Counter />
     </div>
   );
 };
